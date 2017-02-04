b349ba0221e377289ae2546962a4ef8b86076aee
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2017-02-04 15:44:42 +0100

    Adding ability to display movie clip's frames

--- src/main/java/pl/sebce/csfdv/gui/FrameDisplay.java
+++ src/main/java/pl/sebce/csfdv/gui/FrameDisplay.java
@@ -3,24 +3,64 @@ package pl.sebce.csfdv.gui;
 import pl.sebce.csfdv.domain.Project;
 import pl.sebce.csfdv.events.NavigationListener;
 
-import javax.swing.*;
 import java.io.File;
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
-public class FrameDisplay extends JPanel implements NavigationListener {
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+public class FrameDisplay extends JPanel implements NavigationListener, MouseMotionListener {
+
+    private static final long serialVersionUID = 1L;
+
+    private JLabel label = new JLabel();
+
+    private File[] movieClipFrames;
+    private int currentFrameIdx;
     private Project project;
-    private File[] files;
-    private int frameIdx;
+
+    public FrameDisplay() {
+        this.setLayout(new BorderLayout());
+        this.add(label, BorderLayout.CENTER);
+        this.addMouseMotionListener(this);
+    }
 
     public void setProject(Project project) {
         this.project = project;
     }
 
-    public void setFiles(File[] files) {
-        this.files = files;
+    public void setFiles(File[] movieClipFrames) {
+        this.movieClipFrames = movieClipFrames;
     }
 
     public void setFrameIdx(int frameIdx) {
-        this.frameIdx = frameIdx;
+        this.currentFrameIdx = frameIdx;
+        this.repaint();
+    }
+
+    public void repaint() {
+        if (movieClipFrames != null) {
+            File file = movieClipFrames[currentFrameIdx];
+            ImageIcon icon = new ImageIcon(file.getAbsolutePath());
+            label.setIcon(icon);
+        }
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        this.repaint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        this.repaint();
     }
 }
